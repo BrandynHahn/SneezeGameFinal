1c7a6cb5164418e17395f8d1caa932c3ab22f179
JReevesHU <98060331+JReevesHU@users.noreply.github.com>
2022-03-06 18:52:54 -0500

    Player can press space to sneeze

--- SneezeGameFile/Assets/Scripts/PlayerSneezeHit.cs
+++ SneezeGameFile/Assets/Scripts/PlayerSneezeHit.cs
@@ -6,41 +6,15 @@ public class PlayerSneezeHit : MonoBehaviour
 {
     public GameObject SneezeSplatterPrefab;
 
-    //The space in between the sneeze and player
-    public int sneezeSpace;
+    public Transform sneezeArea; 
 
-    //public GameObject Player;
-
-    //private Vector3 savedPosition;
-    
-    //public float newPosition; 
-
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        sneezeSpace = 1; 
-        //savedPosition = Player.transform.position;
-        //newPosition = savedPosition + sneezeSpace;
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown("space"))
         {
-
-            //Add sneezesplatter to the scene
-
-            //GameObject sneeze = Instantiate(SneezeSplatterPrefab) as GameObject;
-
-            //SneezeSplatterPrefab.tranform.position = newPosition;
-
-            Instantiate(SneezeSplatterPrefab, transform.position + (transform.forward), transform.rotation);
-
+            Instantiate(SneezeSplatterPrefab, sneezeArea.position, sneezeArea.rotation);
         }
+        
     }
 
 
