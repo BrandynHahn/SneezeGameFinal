accb3b6784bc48410455bb0426d31d8ef31bd559
JReevesHU <98060331+JReevesHU@users.noreply.github.com>
2022-03-09 14:12:25 -0500

    .

--- SneezeGameFile/Assets/Scripts/Boundaries.cs
+++ SneezeGameFile/Assets/Scripts/Boundaries.cs
@@ -4,25 +4,26 @@ using UnityEngine;
 
 public class Boundaries : MonoBehaviour
 {
+    public Camera MainCamera; 
     public Vector2 screenBounds;
     private float objectWidth;
-    private float objectHeight; 
+    private float objectHeight;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
-        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; //extents = size of width / 2
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; //extents = size of height / 2
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         Vector3 viewPos = transform.position;
-        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x + objectWidth, screenBounds.x * -1 - objectWidth);
-        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y + objectHeight, screenBounds.y * -1 - objectHeight);
-        transform.position = viewPos; 
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
+        transform.position = viewPos;
     }
 }
